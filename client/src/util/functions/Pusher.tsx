8bdc0356cc2e64a9f59915602b1b475d53c257f7
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-05-20 15:06:51 +0700

    up

--- client/src/util/functions/Pusher.tsx
+++ client/src/util/functions/Pusher.tsx
@@ -3,7 +3,7 @@ import PusherClient from 'pusher-js';
 export const pusherClient = new PusherClient('d086b4a963d8b1004838', {
   channelAuthorization: {
     headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },
-    endpoint: 'http://localhost:7000/api/pusher/auth',
+    endpoint: 'https://dev-hub-pro.herokuapp.com/api/pusher/auth',
     transport: 'ajax',
   },
   cluster: 'ap1',
