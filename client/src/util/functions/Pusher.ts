a7890284098034853fa6ff749cf9006dfbd73ecd
ZLoganZ <ln26805@gmail.com>
2023-12-16 23:00:33 +0700

    Update user authentication logic

--- client/src/util/functions/Pusher.ts
+++ client/src/util/functions/Pusher.ts
@@ -3,7 +3,10 @@ import PusherClient from 'pusher-js';
 export const pusherClient = new PusherClient(import.meta.env.VITE_PUSHER_APP_KEY, {
   channelAuthorization: {
     headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` },
-    endpoint: 'https://dev-hub-pro.herokuapp.com/api/pusher/auth',
+    endpoint:
+      (process.env.NODE_ENV === 'development'
+        ? import.meta.env.VITE_SERVER_ENDPOINT
+        : import.meta.env.VITE_SERVER_ENDPOINT_PRODUCT) + '/api/pusher/auth',
     transport: 'ajax',
   },
   cluster: 'ap1',
